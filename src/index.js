649cf8025bdd23fcc17fc98b52e5e29f799880ee
Carolina Pacori <carolina.pacori@gmail.com>
2022-10-13 13:51:29 -0500

    estuve viendo los test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
-import cipher from './cipher.js';
+window.addEventListener("load",Inicio,true)
 
-console.log(cipher);
+function Inicio() {
+    document.getElementById("mensaje").addEventListener("keyup",
+    function () {
+        this.value = this.value.toUpperCase()
+    },
+    true
+    );
+    document.getElementById("cifrar").addEventListener("click",
+    function () {
+        let texto = document.getElementById("mensaje").value;
+        let desplazamiento = document.getElementById("desplazamiento").value;
+        document.getElementById("mensajeCifrado").value = cifrar(texto, desplazamiento);
+        
+    },
+    true)
+
+}
+function cifrar(texto,desplazamiento){
+    let resultado = "";
+    let letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    desplazamiento = (desplazamiento % 26 +26)%26;
+
+    if(texto){
+        for(let i = 0; i<texto.length; i++){
+           if(letras.indexOf(texto[i])!=-1){
+            let posicion = ((letras.indexOf(texto[i])+desplazamiento)%26);
+            resultado += letras[posicion];
+           }  
+           else
+           resultado += texto[i];
+        }
+    }
+    return resultado;
+}
